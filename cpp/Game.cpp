e60ea9ba515b50bf3c49842912ed2bd5d3c2e1fc
Dieter_ntm <nuytemans_dieter@hotmail.com>
2019-04-07 10:39:15 +0200

    Now using const and added an fps counter

--- cpp/Game.cpp
+++ cpp/Game.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <list>
+#include <cmath>
 #include "../Headers/Game.h"
 #include "../Headers/GameObjects/Car.h"
 #include "../Headers/Background.h"
@@ -26,30 +27,48 @@ void Game::Game::start() {
 void Game::gameLoop() {
     //Initiaize general variables
     bool isPlaying = true;
-    int playerVelocity = 5;
     int points = 0;
     int difficulty = 0;
+    //Game settings
+    const int playerVelocity = 5;
+    const int maxCars = 9;
+    const int difficultyIncrement = 2;
+    const int maxDifficulty = 150;
+    const int spawnChance = 200; //Not in %, but the inverse percentage. The actual chance of spawning is 1/spawnChange
+    const int spawnLanes[4] = {108, 218, 333, 443};
+    const int numCarColors = 6; //Must be equal to the size of the Car::Color enum!
+    const int backgroundMoveDownSpeed = 12;
+    const int screenHeight = 640;
+    const double millisecondsPerFrame = 16.7; //17 corresponds to 60 fps
+    const int fpsRefreshRate = 1000;
 
     //Instantiate chrono
     Chrono* chron = new Chrono();
+    Chrono* fpsRefresh = new Chrono();
+    fpsRefresh->startTime();
     srand(time(NULL));
 
     //Initialize the enemy car's list
-    Car* cars [9] = {0};
+    Car* cars [maxCars] = {0};
     int numCars = 0;
 
     //Create score overlay
     TextOverlay* score = factory->createTextOverlay();
     score->setPosition(1,10);
 
+    TextOverlay* diff = factory->createTextOverlay();
+    diff->setPosition(1,50);
+    diff->setText("Difficulty: 0");
+
     //Create lives overlay
     TextOverlay* lives = factory->createTextOverlay();
     lives->setPosition(565,10);
-    lives->setText("Lives: 3");
+    lives->setText("lives: 3");
 
-    TextOverlay* diff = factory->createTextOverlay();
-    diff->setPosition(1,50);
-    diff->setText("Difficulty");
+    //Create lives overlay
+    TextOverlay* fps = factory->createTextOverlay();
+    fps->setPosition(565,50);
+    fps->setText("fps: 60");
 
     //Create background
     Background* bg = factory->createBackground();
@@ -68,37 +87,37 @@ void Game::gameLoop() {
 
         //Calculate score
         score->setText("Score: " + std::to_string(points));
-        diff->setText("Diff: " + std::to_string(difficulty));
+        diff->setText("Difficulty: " + std::to_string(difficulty));
         points++;
 
         //Random car generator
-        if (rand() % (200 - difficulty) == 0 && numCars < 9)
+        if (rand() % (spawnChance - difficulty) == 0 && numCars < maxCars)
         {
-            if (difficulty < 150) difficulty += 2;
+            if (difficulty < maxDifficulty) difficulty += difficultyIncrement;
             //Find the first available place in the enemy car list
             int firstFreeSpot;
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < maxCars; i++)
             {
                 if (cars[i] == 0)
                 {
                     firstFreeSpot = i;
-                    i = 9;
+                    i = maxCars;
                 }
             }
 
             //Pick a random car color
-            Car::Color randomColor = Car::Color(rand() % 6);
+            Car::Color randomColor = Car::Color(rand() % numCarColors);
             //Create a car at a random location with a random velocity
             cars[firstFreeSpot] = factory->createCar(randomColor);
             //x-locations of lanes: 440 330 215 110
-            cars[firstFreeSpot]->setXPos(((rand() % 4)+1)*110-5); cars[firstFreeSpot]->setYPos(-250);
+            cars[firstFreeSpot]->setXPos(spawnLanes[rand() % 4]); cars[firstFreeSpot]->setYPos(-250);
             cars[firstFreeSpot]->setYVelocity(3);
 
             //Find a non-colliding spot to spawn the car
             bool isColliding;
             do {
                 isColliding = false;
-                for (int i = 0; i < 9; i++) {
+                for (int i = 0; i < maxCars; i++) {
                     if (i != firstFreeSpot && cars[i] != 0) {
                         if (cars[firstFreeSpot]->isColliding(cars[i]))
                         {
@@ -146,19 +165,19 @@ void Game::gameLoop() {
         }
 
         //Handle background
-        bg->moveDown(12);
+        bg->moveDown(backgroundMoveDownSpeed);
         if (bg->getLocation() >= 0) bg->resetLocation();
 
         /**
-         * Move objects & render enemy cars
+         * Move objects, perform collision detection & render enemy cars
          */
         factory->startRendering();
         bg->visualize();
-        for (int i = 0; i < 9; i++)
+        for (int i = 0; i < maxCars; i++)
         {
             if (cars[i] != 0)
             {
-                if (cars[i]->getYPos() > 640)
+                if (cars[i]->getYPos() > screenHeight)
                 {
                     delete cars[i];
                     cars[i] = 0;
@@ -189,10 +208,15 @@ void Game::gameLoop() {
         score->render();
         lives->render();
         diff->render();
-        factory->finishRendering();
 
-        //17 corresponds to 60 fps
-        while (chron->getTimePassed() < 17) {}
+
+        while (chron->getTimePassed() < millisecondsPerFrame) {}
+        if (fpsRefresh->getTimePassed() >= fpsRefreshRate) {
+            fps->setText("fps: " + std::to_string( (int) round(1000 / chron->getTimePassed()) ));
+            fpsRefresh->startTime();
+        }
+        fps->render();
+        factory->finishRendering();
     }
 }
 
